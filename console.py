49e2503c79ff1ad17935385bffc269dd81b0dba4
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-10 16:08:17 +0000

    a

--- console.py
+++ console.py
@@ -28,5 +28,5 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
-    if __name__ == '__main__':
-        HBNBCommand().cmdloop()
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
