9e29d31f9092a02fd184ace6a4f458062dfae65f
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-11 00:05:33 +0000

    new commit

--- tests/test_console.py
+++ tests/test_console.py
@@ -20,47 +20,47 @@ class Test_Console(unittest.TestCase):
         """
         self.assertEqual(result, f.getvalue())
 
-        def test_do_quit(self):
-            """Tests the quit command"""
-            with patch("sys.stdout", new=StringIO()) as f:
-                HBNBCommand().onecmd()("quit")
-            result = f.getvalue()
-            self.asserTrue(len(result) == 0)
-            self.assertEqual("", result)
+    def test_do_quit(self):
+        """Tests the quit command"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd()("quit")
+        result = f.getvalue()
+        self.asserTrue(len(result) == 0)
+        self.assertEqual("", result)
 
-            with patch("stdout", new=StringIO()) as f:
-                HBNBCommand().oncmd("quit terminal")
-            result = f.getvalue()
-            self.assertTrue(len(result) == 0)
-            self.assertEqual("", result)
+        with patch("stdout", new=StringIO()) as f:
+            HBNBCommand().oncmd("quit terminal")
+        result = f.getvalue()
+        self.assertTrue(len(result) == 0)
+        self.assertEqual("", result)
 
-       def test_do_EOF(self):
-            """Tests the EOF command"""
-            with patch("sys.stdout", new=StringIO()) as f:
-                HBNBCommand().onecmd()("EOF")
-            result = f.getvalue()
-            self.asserTrue(len(result) == 0)
-            self.assertEqual("", result)
+    def test_do_EOF(self):
+        """Tests the EOF command"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd()("EOF")
+        result = f.getvalue()
+        self.asserTrue(len(result) == 0)
+        self.assertEqual("", result)
 
-            with patch("stdout", new=StringIO()) as f:
-                HBNBCommand().oncmd("EOF terminal")
-            result = f.getvalue()
-            self.assertTrue(len(result) == 0)
-            self.assertEqual("", result)
+        with patch("stdout", new=StringIO()) as f:
+            HBNBCommand().oncmd("EOF terminal")
+        result = f.getvalue()
+        self.assertTrue(len(result) == 0)
+        self.assertEqual("", result)
 
-        def test_do_emptyline(self):
-            """Tests the emptyline command"""
-            with patch("sys.stdout", new=StringIO()) as f:
-                HBNBCommand().onecmd()("\n")
-            result = f.getvalue()
-            self.asserTrue(len(result) == 0)
-            self.assertEqual("", result)
+    def test_do_emptyline(self):
+        """Tests the emptyline command"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd()("\n")
+        result = f.getvalue()
+        self.asserTrue(len(result) == 0)
+        self.assertEqual("", result)
 
-            with patch("stdout", new=StringIO()) as f:
-                HBNBCommand().oncmd("             \n")
-            result = f.getvalue()
-            self.assertTrue(len(result) == 0)
-            self.assertEqual("", result)
+        with patch("stdout", new=StringIO()) as f:
+            HBNBCommand().oncmd("             \n")
+        result = f.getvalue()
+        self.assertTrue(len(result) == 0)
+        self.assertEqual("", result)
  
 if __name__ == "__main__":
     unittest.main()
